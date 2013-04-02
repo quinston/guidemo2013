8fd43d2837e54f452287d8b82e8d01b37e47d987
quinston <lklq@live.com>
2013-04-02 09:17:06 -0400

    Blank stuff. And a makefile.

---
+++ Guidemo2013.java
@@ -0,0 +1,16 @@
+/*
+Quincy Lam, Ryan Marks, Stephen Wen
+
+*/
+
+
+
+import javax.swing.JLabel;
+import javax.swing.JFrame;
+
+public class Guidemo2013 {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ Makefile
@@ -0,0 +1,2 @@
+Guidemo2013.class: Guidemo2013.java ButtonListener.java CheckboxListener.java RadioListener.java
+	javac Guidemo2013.java
