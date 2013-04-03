46ce7233753af8310633e1c2159555955eaad29c
Ryan Marks <ryanmarks151@gmail.com>
2013-04-03 10:44:43 -0400

    Wrote Checkbox Blind. Modified main and button to allow compilation
    CheckboxListener.java was written blind and is untested. Do not trust
    it.
    
    Button listener had just enough code added to allow compilation.
    
    Many lines had to be commented as they reference the unwritten button
    class.

--- CheckboxListener.java
+++ CheckboxListener.java
@@ -1,8 +1,39 @@
-package start;
-
-import javax.swing.*;Ê
-public class CheckboxListener{
-	
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.lang.Math;
+public class CheckboxListener extends JToggleButton{
+		private 
+		int size;
+		JCheckBox boxes[];
 	CheckboxListener(){
+		size=(int)Math.random()*9+1;
+		boxes = new JCheckBox[size];
+		
+		for (int i=0;i<size;i++){
+		
+		boxes[i] = new JCheckBox(""+i);
+		boxes[i].setMnemonic(KeyEvent.VK_C); 
+		boxes[i].setSelected(true);
+		
+		//boxes[i].addItemListener(this);
+		}
+
+	
+
 	}
-}
+	
+	
+	 public void itemStateChanged(ItemEvent e) {
+		Object source = e.getItemSelectable();
+		
+		int box;
+	    for (int i=0;i<size;i++){
+				//if (box == boxes[i]) {
+	    	box=i;
+	    	break;//}
+	    }
+//		if (e.getStateChange() == ItemEvent.DESELECTED)
+
+	}
+};
--- Guidemo2013.java
+++ Guidemo2013.java
@@ -1,28 +1,29 @@
 /*
 Quincy Lam, Ryan Marks, Stephen Wen
-
 */
-
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.JLabel;
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.swing.*;
 
 public class Guidemo2013 implements ActionListener {
 	public static void main(String[] args) {
 		JFrame f = new JFrame("Button Checkbox Radio Demo");
 		f.getContentPane().add(new CheckboxListener());	
-		ButtonListener b1 = new ButtonListener("Continue");
-		b1.addChangeListener(this);
-		f.getContentPane().add(b1);
+		//ButtonListener b1 = new ButtonListener("Continue");
+		//b1.addChangeListener(this);
+		//f.getContentPane().add(b1);
 
 
 		f.getContentPane().add(new CheckboxListener());	
-		ButtonListener b2 = new ButtonListener("Continue");
-		b2.addChangeListener(this);
-		f.getContentPane().add(b2);
+		//ButtonListener b2 = new ButtonListener("Continue");
+		//b2.addChangeListener(this);
+		//f.getContentPane().add(b2);
 
-		
 		f.pack();
+		f.setVisible(true);
 	}
 
 	public void actionPerformed (ActionEvent e) {
